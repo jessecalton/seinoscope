c71832d0448192b91af31f9041bf122f5fc47791
Jesse Calton <jessecalton@gmail.com>
2020-10-02 21:48:25 -0700

    adds dropdown menus for date selection, state

--- client/src/components/DatePicker.js
+++ client/src/components/DatePicker.js
@@ -1,27 +1,33 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import Form from 'react-bootstrap/Form';
 
 const DatePicker = () => {
   const [years, setYears] = useState([]);
+  const [days, setDays] = useState([]);
+  const [year, setYear] = useState(null);
+  const [month, setMonth] = useState('January');
 
   useEffect(() => {
-    getYears();
+    const currentYear = new Date().getFullYear();
+    getYears(currentYear);
+    setYear(currentYear);
+    getDaysInMonth(month, currentYear);
   }, []);
 
-  const getYears = () => {
-    let currentYear = new Date().getFullYear();
+  // Takes the current year, and gets the most recent 120 calendar years
+  const getYears = (currentYear) => {
     let selectableYears = [];
     for (
-      var yearBoundary = currentYear - 120;
+      var yearBoundary = currentYear - 121;
       currentYear > yearBoundary;
       currentYear--
     ) {
       selectableYears.push(currentYear);
-      // setYears((prevState) => [...prevState, yearBoundary]);
     }
     setYears(selectableYears);
   };
 
+  // Our 12 calendar months
   const monthMap = [
     'January',
     'February',
@@ -37,23 +43,59 @@ const DatePicker = () => {
     'December',
   ];
 
+  // Once a year and month are selected, sets array of valid dates available
+  // for a chosen month. Handles leap years.
+  const getDaysInMonth = (newMonth, newYear) => {
+    const selectedMonth = monthMap.indexOf(newMonth) + 1;
+    const days = new Date(newYear, selectedMonth, 0).getDate();
+
+    setDays([...Array(days + 1).keys()].slice(1));
+  };
+
+  // If new month is selected, updates available valid dates.
+  const onMonthChange = (e) => {
+    setMonth(e.target.value);
+    getDaysInMonth(e.target.value, year);
+  };
+
+  // If new year is selected, updates available valid dates.
+  // Really only matters for leap years if february is chosen month.
+  const onYearChange = (e) => {
+    setYear(e.target.value);
+    getDaysInMonth(month, e.target.value);
+  };
+
   return (
     <div>
       <Form>
         <Form.Row>
-          <Form.Group controlId='exampleForm.SelectCustom'>
+          <Form.Group controlId='selectYear'>
             <Form.Label>Year</Form.Label>
-            <Form.Control as='select' custom>
+            <Form.Control as='select' custom onChange={onYearChange}>
               {years.map((year) => (
-                <option>{year}</option>
+                <option id={year} value={year}>
+                  {year}
+                </option>
               ))}
             </Form.Control>
           </Form.Group>
-          <Form.Group controlId='exampleForm.SelectCustom'>
+          <Form.Group controlId='selectMonth'>
             <Form.Label>Month</Form.Label>
-            <Form.Control as='select' custom>
+            <Form.Control as='select' custom onChange={onMonthChange}>
               {monthMap.map((month) => (
-                <option>{month}</option>
+                <option id={month} value={month}>
+                  {month}
+                </option>
+              ))}
+            </Form.Control>
+          </Form.Group>
+          <Form.Group controlId='selectDay'>
+            <Form.Label>Day</Form.Label>
+            <Form.Control as='select' custom>
+              {days.map((day) => (
+                <option id={day} value={day}>
+                  {day}
+                </option>
               ))}
             </Form.Control>
           </Form.Group>
