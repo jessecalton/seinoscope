6f931064b16e9929b4e1f361ce362f7905e9b706
Jesse Calton <jessecalton@Jesses-MacBook-Pro.local>
2024-10-20 11:07:47 -0700

    fixes proxy issues, updates modules

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ if (process.env.NODE_ENV === 'production') {
   // This is saying "look at the current directory, then look at client, then build, then load index.html"
 }
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
